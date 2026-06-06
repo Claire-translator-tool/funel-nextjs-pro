c1559cd3b26999a199957abb51f7f600766e0702
Claire-translator-tool <13759983480@139.com>
2026-06-06 11:54:52 +0800

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/admin', '/private'] }],
-    sitemap: 'https://your-domain.com/sitemap.xml',
+    sitemap: 'https://funelsensor.com/sitemap.xml',
   }
 }
