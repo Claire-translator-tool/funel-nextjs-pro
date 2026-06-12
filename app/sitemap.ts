f73f0aa499197be43b6b4000879099b739f9f093
Claire-translator-tool <13759983480@139.com>
2026-06-12 10:24:35 +0800

    Update homepage layout and Chinese page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,11 +4,16 @@ import { getProducts } from "./products/product-data";
 import { getSiteSettings } from "./site-settings";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const [site, pages, products] = await Promise.all([getSiteSettings(), getPublishedPages(), getProducts()]);
+  const [site, pages, products] = await Promise.all([
+    getSiteSettings(),
+    getPublishedPages(),
+    getProducts(),
+  ]);
   const paths = new Set<string>();
 
   pages.forEach((page) => paths.add(pagePath(page.slug)));
   paths.add("");
+  paths.add("/zh");
   paths.add("/products");
   paths.add("/contact");
   products.forEach((product) => paths.add(`/products/${product.slug}`));
