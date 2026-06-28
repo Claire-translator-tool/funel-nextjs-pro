35b78c984bd4688afae00e94684dc5763f6a6097
Claire-translator-tool <13759983480@139.com>
2026-06-29 04:08:59 +0800

    Use www sitemap URL in robots

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import type { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: '*', allow: '/', disallow: ['/admin', '/private'] }],
-    sitemap: 'https://funelsensor.com/sitemap.xml',
+    sitemap: 'https://www.funelsensor.com/sitemap.xml',
   }
 }
